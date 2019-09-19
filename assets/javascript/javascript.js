a54b252dbe990ce65bc78a730621cd361df1a414
Nick Frattaroli <Nafratt@gmail.com>
2019-09-19 18:08:13 -0400

    bug fix attempt

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,35 +2,27 @@ var topics= ["Cats", "Dogs", "Birds", "Snakes"];
 
 
     $("#buttons-view"). on("click",function(){
-      var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=kitten";
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q="+
+      topics +"&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
       $.ajax({
         url: queryURL,
         method: "GET"
       }) 
       .then(function(response){
-        var imageUrl = response.data.image_original_url;
-
+        var imageUrl = Response.data;
           //setting up the cat image var and giving it an image tag 
-          var catImage = $("<img>");
+          var aniImage = $("<img>");
 
           // giving our   var an attribute of source and the image we pulled/ assinging each image the alt cat image 
-          catImage.attr("src", imageUrl);
-          catImage.attr("alt", "cat image");
+          aniImage.attr("src", imageUrl);
+          aniImage.attr("alt", "animal image");
+          
+
 
           //telling it to write the image to the page before the one that was priviously pulled  after the img tag has been applied 
-          $("#images").prepend(catImage)
-      });
-    });
-      function buttonspos() {
-        $('#buttons-view').empty();
-        for (var i = 0; i < topics.length; i++) {
-          var b = $("<button>")
-          b.addClass('animal');
-          b.attr('data-name', topics[i]);
-          b.text(topics[i])
-          $("#buttons-view").append(b);
-        }
-      }
+          $("#gifs-go-here").prepend(aniImage)
+      
+  
       $("#add-animal").on("click", function (event) {
         
         event.preventDefault();
@@ -40,5 +32,35 @@ var topics= ["Cats", "Dogs", "Birds", "Snakes"];
         topics.push(cute);
         buttonspos();
     });
+    // function writed(){
+      
 
+      });
+  });
+ 
+    $(".gif").on("click", function() {
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
+  
+    function buttonspos() {
+      $('#buttons-view').empty();
+      for (var i = 0; i < topics.length; i++) {
+        var b = $("<button>")
+        b.addClass('animal');
+        b.attr('data-name', topics[i]);
+        b.text(topics[i])
+        $("#buttons-view").append(b);
+      }
+    }
+  // writed();
     buttonspos();
